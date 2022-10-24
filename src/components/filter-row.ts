914086a004cf01e8ce2102ea52395a45e609fa51
desig9stein <mspopovv@gmail.com>
2022-10-24 14:55:18 +0300

    style(filtering): update styles

--- src/components/filter-row.ts
+++ src/components/filter-row.ts
@@ -317,14 +317,19 @@ export default class ApexFilterRow<T extends object> extends LitElement {
 
   protected renderFilterState(column: ColumnConfig<T>) {
     return html`
-      <igc-button
-        variant="flat"
+      <igc-chip
         @click=${() => {
           this.column = column;
           this.expression = this.#defaultExpression;
           this.#show();
         }}
-        >Filter</igc-button
+      >
+        <igc-icon
+          slot="prefix"
+          name="filter"
+          collection="internal"
+        ></igc-icon>
+        Filter</igc-chip
       >
     `;
   }
