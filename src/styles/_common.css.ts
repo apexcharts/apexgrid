57b2fb517df408d6d70923ead110e573d24bb6de
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2025-07-07 18:30:19 +0300

    chore: Applied ignored files changes

--- src/styles/_common.css.ts
+++
@@ -1,2 +0,0 @@
-import { css } from 'lit';
-export const styles = css`:host{--is-large: clamp(0, (var(--component-size, 1) + 1) - var(--ig-size-large, 3), 1);--is-medium: min( clamp(0, (var(--component-size, 1) + 1) - var(--ig-size-medium, 2), 1), clamp(0, var(--ig-size-large, 3) - var(--component-size, 1), 1) );--is-small: clamp(0, var(--ig-size-medium) - var(--component-size, 1), 1);--component-size: 3}`;
